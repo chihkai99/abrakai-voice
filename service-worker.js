338b262e4527a64b632d874dbd72e484bf1e6a27
Abrakai <vincentyu.pic@gmail.com>
2026-07-07 13:54:56 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'abrakai-cache-v5.4';
-const urlsToCache = ['./index.html?v=5.4','./manifest.json?v=5.4'];
+const CACHE_NAME = 'abrakai-cache-v5.5';
+const urlsToCache = ['./index.html?v=5.5','./manifest.json?v=5.5'];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -32,7 +32,7 @@ self.addEventListener('fetch', event => {
         const clone = res.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(req, clone)).catch(() => undefined);
         return res;
-      }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html?v=5.4')))
+      }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html?v=5.5')))
     );
     return;
   }
