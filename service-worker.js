a2ebc7ce54847c5139bedc46780aeaf760788c00
Abrakai <vincentyu.pic@gmail.com>
2026-07-13 01:24:02 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'soundnoteai-cache-v6.0.0-beta5';
-const urlsToCache = ['./index.html?v=6.0.0-beta4','./manifest.json?v=6.0.0-beta4'];
+const CACHE_NAME = 'soundnoteai-cache-v6.0.0-beta6';
+const urlsToCache = ['./index.html?v=6.0.0-beta6','./manifest.json?v=6.0.0-beta6'];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
